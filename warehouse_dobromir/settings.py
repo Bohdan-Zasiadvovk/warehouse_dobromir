7631d45e5e4c4a52f614ddf6e89afb67b0bc9877
Bohdan Zasiadvovk <zasadvovkb@gmail.com>
2024-01-13 18:20:17 +0200

    Add six apps in INSTALLED_APPS

--- warehouse_dobromir/settings.py
+++ warehouse_dobromir/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ingredient',
+    'additional_product',
+    'container',
+    'package',
+    'recipe',
+    'warehouse',
+
+
 ]
 
 MIDDLEWARE = [
