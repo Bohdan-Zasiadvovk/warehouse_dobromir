a7e2def54180ed581c1132476b8a4d0aab052e63
Bohdan Zasiadvovk <zasadvovkb@gmail.com>
2024-01-14 16:46:51 +0200

    Add IngredientForm

---
+++ ingredient/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Ingredient
+
+
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ['name', 'measure']
