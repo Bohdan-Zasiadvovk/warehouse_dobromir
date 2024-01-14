0f9a90a59ed1ea4a20331133033f069faa5bf337
Bohdan Zasiadvovk <zasadvovkb@gmail.com>
2024-01-14 14:18:01 +0200

    Update display in django admin

--- warehouse/admin.py
+++ warehouse/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Warehouse
+from .models import Warehouse, WarehouseRecordItem
 
 
 admin.site.register(Warehouse)
+admin.site.register(WarehouseRecordItem)
