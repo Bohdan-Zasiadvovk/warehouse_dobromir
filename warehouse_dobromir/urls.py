0345337ad53c19757a2984b2b59f747f80c8aa7b
Bohdan Zasiadvovk <zasadvovkb@gmail.com>
2024-01-15 19:42:51 +0200

    Add urls for ingredient and package

--- warehouse_dobromir/urls.py
+++ warehouse_dobromir/urls.py
@@ -15,14 +15,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from ingredient import views
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+from ingredient import views as ingredient_views
+from package import views as package_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('ingredient/create/', views.create_ingredient, name='create_ingredient'),
-    path('ingredient/all/', views.get_all_ingredients, name='get_all_ingredients'),
-    path('ingredient/update/<int:id>/', views.update_ingredient, name='update_ingredient'),
+
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
+    path('ingredient/', ingredient_views.ingredient, name='ingredient'),
+    path('ingredient/create/', ingredient_views.create_ingredient, name='create_ingredient'),
+    path('ingredient/all/', ingredient_views.get_all_ingredients, name='all_ingredients'),
+    path('ingredient/update/<ingredient_id>/', ingredient_views.update_ingredient, name='update_ingredient'),
+    path('ingredient/delete/<ingredient_id>/', ingredient_views.delete_ingredient, name='delete_ingredient'),
+
+    path('package/', package_views.package, name='package'),
+    path('package/create/', package_views.create_package, name='create_package'),
+    path('package/all/', package_views.get_all_packages, name='all_packages'),
+    path('package/update/<package_id>/', package_views.update_package, name='update_package'),
+    path('package/delete/<package_id>/', package_views.delete_package, name='delete_package'),
+
+
 
 ]
