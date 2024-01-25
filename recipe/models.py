c565898e86191f4971d423a625d64921129a18d3
Bohdan Zasiadvovk <zasadvovkb@gmail.com>
2024-01-26 00:57:29 +0200

    Update models

--- recipe/models.py
+++ recipe/models.py
@@ -10,13 +10,19 @@ class Recipe(models.Model):
 
 
 class RecipeItem(models.Model):
-    ingredient = models.ForeignKey(Item, on_delete=models.CASCADE)
-    recipe_item = models.ForeignKey('Recipe', on_delete=models.CASCADE)
-    measure = models.CharField(max_length=20)
+    MEASURE_CHOICES = [
+        ('gram', 'грам'),
+        ('centimeter', 'сантиметр'),
+        ('piece', 'штук')
+    ]
+
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    measure = models.CharField(max_length=20, choices=MEASURE_CHOICES)
     quantity = models.DecimalField(max_digits=10, decimal_places=6)
 
     def __str__(self):
-        return f"{self.ingredient.name} -- {self.quantity} ({self.measure})"
+        return f"{self.item.name} -- {self.quantity} ({self.measure})"
 
 
 
