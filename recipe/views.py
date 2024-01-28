d0a49a03a39ec4afc345671854f0c5d8744c7cfe
Bohdan Zasiadvovk <zasadvovkb@gmail.com>
2024-01-28 19:23:40 +0200

    Update create_recipe, delete unnecessary views

--- recipe/views.py
+++ recipe/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import RecipeForm, RecipeItemForm
 from .models import Recipe, RecipeItem
+from item.models import Item
+
 
 
 def recipe(request):
@@ -8,82 +9,21 @@ def recipe(request):
 
 
 def create_recipe(request):
-    if request.method == 'GET':
-        return render(request, 'recipe/createrecipe.html', {'form': RecipeForm()})
-    else:
-        try:
-            form = RecipeForm(request.POST)
-            new_recipe = form.save(commit=False)
-            new_recipe.save()
-            return redirect('all_recipes')
-        except ValueError:
-            return render(request, 'recipe/createrecipe.html', {'form': RecipeForm(), 'error': 'Bad data passed in. Try again'})
-
-
-def get_all_recipes(request):
-    if request.method == "GET":
-        recipes = Recipe.objects.all()
-        return render(request, 'recipe/allrecipes.html', {'recipes': recipes})
-
-
-def update_recipe(request, recipe_id):
-    recipe = get_object_or_404(Recipe, id=recipe_id)
-    if request.method == "GET":
-        return render(request, 'recipe/updaterecipe.html', {'recipe': recipe, 'form': RecipeForm(instance=recipe)})
-    else:
-        try:
-            form = RecipeForm(request.POST, instance=recipe)
-            form.save()
-            return redirect('all_recipes')
-        except ValueError:
-            return render(request, 'recipe/updaterecipe.html', {'recipe': recipe, 'form': RecipeForm(instance=recipe)})
-
-
-def delete_recipe(request, recipe_id):
-    recipe = get_object_or_404(Recipe, id=recipe_id)
-
     if request.method == 'POST':
-        recipe.delete()
-        return redirect('all_recipes')
+        recipe_name = request.POST.get('recipe_name')
+        items = request.POST.getlist('item')
+        measures = request.POST.getlist('measure')
+        quantities = request.POST.getlist('quantity')
+
+        recipe = Recipe.objects.create(name=recipe_name)
+
+        for item, measure, quantity in zip(items, measures, quantities):
+            item_obj = Item.objects.get(pk=item)
+            RecipeItem.objects.create(recipe=recipe, item=item_obj, measure=measure, quantity=quantity)
+
+        return redirect('recipe')  # Перенаправлення на іншу сторінку, наприклад, список рецептів
+
+    items = Item.objects.all()
+    return render(request, 'recipe/createrecipe.html', {'items': items})
 
 
-def create_recipe_item(request, recipe_id):
-    recipe = get_object_or_404(Recipe, id=recipe_id)
-
-    if request.method == 'GET':
-        return render(request, 'recipe/createrecipeitem.html', {'form': RecipeItemForm(), 'recipe': recipe})
-
-    else:
-        try:
-            form = RecipeItemForm(request.POST)
-            recipe_item = form.save(commit=False)
-            recipe_item.recipe_item = recipe
-            recipe_item.save()
-            return redirect('recipe_detail', recipe_id=recipe.id)
-        except ValueError:
-            return render(request, 'recipe/createrecipeitem.html', {'form': RecipeItemForm(),  'error': 'Bad data passed in. Try again'})
-
-def recipe_detail(request, recipe_id):
-    recipe = get_object_or_404(Recipe, id=recipe_id)
-    return render(request, 'recipe/recipedetail.html', {'recipe': recipe})
-
-
-def update_recipe_item(request, recipe_id, recipe_item_id):
-    recipe_item = get_object_or_404(RecipeItem, id=recipe_item_id)
-    if request.method == "GET":
-        return render(request, 'recipe/updaterecipeitem.html', {'recipeitem': recipe_item, 'form': RecipeItemForm(instance=recipe_item)})
-    else:
-        try:
-            form = RecipeItemForm(request.POST, instance=recipe_item)
-            form.save()
-            return redirect('recipe_detail', recipe_id=recipe_id)
-        except ValueError:
-            return render(request, 'recipe/updaterecipeitem.html', {'recipeitem': recipe_item, 'form': RecipeItemForm(instance=recipe_item)})
-
-
-def delete_recipe_item(request, recipe_id, recipe_item_id):
-    recipe_item = get_object_or_404(RecipeItem, id=recipe_item_id)
-
-    if request.method == 'POST':
-        recipe_item.delete()
-        return redirect('recipe_detail', recipe_id=recipe_id)
