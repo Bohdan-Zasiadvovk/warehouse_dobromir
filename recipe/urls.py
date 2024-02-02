b0cc7f362cc96ff3ae5d9e081e36520c06a4c59a
Bohdan Zasiadvovk <zasadvovkb@gmail.com>
2024-02-02 03:36:24 +0200

    Add paths for: create_recipe, all_recipes, recipe_detail, update_recipe, delete_recipe

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    path('', views.create_recipe, name='create_recipe'),
-    # path('create/', views.create_recipe, name='create_recipe'),
+    path('', views.recipe, name='recipe'),
+    path('create/', views.create_recipe, name='create_recipe'),
+    path('all', views.all_recipes, name='all_recipes'),
+    path('<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
+    path('<int:recipe_id>/update/', views.update_recipe, name='update_recipe'),
+    path('<int:recipe_id>/delete/', views.delete_recipe, name='delete_recipe'),
+
 ]
